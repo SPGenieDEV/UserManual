15bf51f7c6fea7ca5b1a4b287564297f0762584f
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2023-05-17 00:32:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { Installationback } from './components/pages/Installationback';
 import { Installationjira } from './components/pages/installationjira';
 import { Installationchrome } from './components/pages/installationchrome';
 import { Usermanualfront } from './components/pages/usermanualfront';
+import { Usermanualjira } from './components/pages/usermanualjira';
+import { Usermanualchrome } from './components/pages/usermanualchrome';
 
 function App() {
   return (
@@ -14,12 +16,14 @@ function App() {
       <ChakraProvider>
         <Router>
           <Routes>
-            <Route path="/" element={<Homepage />} />
+            <Route exact path="/" element={<Homepage />} />
             <Route path="/installation/spgenie-tool-frontend" element={<Installationfront />} />
             <Route path="/installation/spgenie-tool-backend" element={<Installationback />} />
             <Route path="/installation/jira-plugin" element={<Installationjira />} />
             <Route path="/installation/chrome-extension" element={<Installationchrome />} />
             <Route path="/user-manual/spgenie-tool-frontend" element={<Usermanualfront />} />
+            <Route path="/user-manual/jira-plugin" element={<Usermanualjira />} />
+            <Route path="/user-manual/chrome-extension" element={<Usermanualchrome />} />
           </Routes>
         </Router>
       </ChakraProvider>
